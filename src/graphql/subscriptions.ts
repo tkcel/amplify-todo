0fb42aa1e8b336a25400f4468f835368f2703261
tkcel <team.vals.1.1.0@gmail.com>
2020-12-21 22:19:46 +0900

    amplify add api

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,46 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      name
+      status
+      deadLine
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      name
+      status
+      deadLine
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      name
+      status
+      deadLine
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
